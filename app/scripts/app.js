9d22a9b8961f8364fd5de0eee7bf5f0359f4cb78
Tomas Huang <huangguan3112789@gmail.com>
2014-03-19 19:16:42 +0800

    Init.

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('MobileCMSApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
